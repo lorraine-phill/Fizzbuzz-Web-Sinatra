1690c7ff98091737c2d5bdbae55590914561ba83
lorraine-phill <lp3767-web@yahoo.com>
2013-09-19 18:10:38 +0100

    update url

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
     require 'sinatra'
     require_relative 'fizz_meth2'
 
-    get '/fizzbuzz' do
-      erb :index 
+    get '/' do
+      erb :index
+
     end
 
-    get '/fizzbuzz/result' do
+    get '/result' do
         num1 = params[:num1].to_i
         num2 = params[:num2].to_i
         @fizzbuzz = FizzBuzz.new(num1,num2)
